e884129ec479c8b4caeb2004132a41a6408c8b71
Toon Kling <t.kling@student.tudelft.nl>
2023-03-16 21:42:45 +0100

    tmp

--- src/weblab/Solution.java
+++ src/weblab/Solution.java
@@ -5,216 +5,26 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 class Solution {
-    public static List<String> getBinaryStrings(int n) {
-
-        //Binary Strings
-        Solver<Character> mySolver = new Solver<>();
-        Character[][] domain = new Character[n][];
-        for (int i = 0; i < n; i++) {
-            domain[i] = new Character[] {'0', '1'};
-        }
-        List<Lambda<Boolean, List<Character>>> constraints = new ArrayList<>();
-        Set<List<Character>> known_solutions = new HashSet<>();
-        Lambda<Boolean, List<Character>> newConstraint = x -> {
-            if (!known_solutions.contains(x)) {
-                known_solutions.add(x);
-                return false;
-            }
-            return true;
-        };
-        constraints.add(newConstraint);
-
-        List<List<Character>> all_solutions = mySolver.getAllSolutions(domain, constraints);
-
-        // Collect the result and convert it to the correct datastructure.
-        List<String> finalSolution = all_solutions.stream().map(characters ->
-                characters.stream().map(Object::toString).reduce((acc, e) -> acc + e).get()
-        ).sorted().collect(Collectors.toList());
-
-        System.out.println("All solutions: " + finalSolution);
-        return finalSolution;
-    }
-
-    public static List<String> permutationsNoRepetitions(int n, int k) {
-        Solver<Integer> mySolver = new Solver<>();
-
-        Integer[][] domain = new Integer[k][n];
-
-        for (int i = 0; i < k; i++) {
-            Integer[] choice = new Integer[n];
-            for (int j = 0; j<n; j++) {
-                choice[j] = j+1;
-            }
-            domain[i] = choice;
-        }
-
-        List<Lambda<Boolean, List<Integer>>> constraints = new ArrayList<>();
-
-        Lambda<Boolean, List<Integer>> lambda = x -> {
-            HashSet<Integer> in = new HashSet<>();
-            for(int i : x) {
-                if(in.contains(i)) {
-                    return true;
-                }
-                in.add(i);
-            }
-            return false;
-        };
-        constraints.add(lambda);
-
-        Set<Set<Integer>> known_solutions = new HashSet<>();
-        Lambda<Boolean, List<Integer>> newConstraint = x -> {
-            Set<Integer> set = new HashSet<>(x);
-            if (!known_solutions.contains(set)) {
-                known_solutions.add(set);
-                return false;
-            }
-            return true;
-        };
-        constraints.add(newConstraint);
-
-        List<List<Integer>> all_solutions = mySolver.getAllSolutions(domain, constraints);
-
-        // Collect the result and convert it to the correct datastructure.
-        List<String> finalSolution = all_solutions.stream().map(characters ->
-                characters.stream().map(Object::toString).reduce((acc, e) -> acc + e).get()
-        ).sorted().collect(Collectors.toList());
-
-        System.out.println("All solutions: " + finalSolution);
-        return finalSolution;
-    }
-
-    public static List<String> permutationsWithRepetitions(int n, int k) {
-        Solver<Integer> mySolver = new Solver<>();
-
-        Integer[][] domain = new Integer[k][n];
-
-        for (int i = 0; i < k; i++) {
-            Integer[] choice = new Integer[n];
-            for (int j = 0; j<n; j++) {
-                choice[j] = j+1;
-            }
-            domain[i] = choice;
-        }
-
-        List<Lambda<Boolean, List<Integer>>> constraints = new ArrayList<>();
-
-        Lambda<Boolean, List<Integer>> lambda = x -> {
-            HashSet<Integer> in = new HashSet<>();
-            for(int i : x) {
-                if(in.contains(i)) {
-                    return true;
-                }
-                in.add(i);
-            }
-            return false;
-        };
-        constraints.add(lambda);
-
-        Set<List<Integer>> known_solutions = new HashSet<>();
-        Lambda<Boolean, List<Integer>> newConstraint = x -> {
-            if (!known_solutions.contains(x)) {
-                known_solutions.add(x);
-                return false;
-            }
-            return true;
-        };
-        constraints.add(newConstraint);
-
-        List<List<Integer>> all_solutions = mySolver.getAllSolutions(domain, constraints);
-
-        // Collect the result and convert it to the correct datastructure.
-        List<String> finalSolution = all_solutions.stream().map(characters ->
-                characters.stream().map(Object::toString).reduce((acc, e) -> acc + e).get()
-        ).sorted().collect(Collectors.toList());
-
-        System.out.println("All solutions: " + finalSolution);
-        return finalSolution;
-    }
-
-    public static List<String> subSets(int n) {
+    public static List<Integer> exampleProblem(int n) {
 
         //Binary Strings
         Solver<Integer> mySolver = new Solver<>();
         Integer[][] domain = new Integer[n][];
         for (int i = 0; i < n; i++) {
-            domain[i] = new Integer[] {-1, i+1};
+            domain[i] = new Integer[] {0, 1};
         }
-        List<Lambda<Boolean, List<Integer>>> constraints = new ArrayList<>();
-        Set<Set<Integer>> known_solutions = new HashSet<>();
-        Lambda<Boolean, List<Integer>> newConstraint = x -> {
-            Set<Integer> set = new HashSet<>(x);
-            if(set.contains(-1)) {
-                set.remove(-1);
-            }
-            if(set.isEmpty()) return true;
-            if (!known_solutions.contains(set)) {
-                known_solutions.add(set);
-                return false;
-            }
-            return true;
-        };
-        constraints.add(newConstraint);
+        List<Constraint<List<Integer>, Boolean>> constraints = new ArrayList<>();
+        constraints.add(new Constraint<>((a) -> a.get(0) - a.get(2) >= 0, new int[]{0, 2}));
+        constraints.add(new Constraint<>((a) -> a.get(1) != 0, new int[]{1}));
 
-        List<List<Integer>> all_solutions = mySolver.getAllSolutions(domain, constraints);
-        System.out.println("hi");
-        // Collect the result and convert it to the correct datastructure.
-        List<String> finalSolution = known_solutions.stream().map(characters ->
-                characters.stream().map(Object::toString).reduce((acc, e) -> acc + e).get()
-        ).sorted().collect(Collectors.toList());
-        Collections.sort(finalSolution);
-        System.out.println("All solutions: " + finalSolution);
-        return finalSolution;
+        List<Integer> solution = mySolver.getOneSolution(domain, constraints);
+
+        System.out.println("A solution: " + solution);
+        return solution;
     }
 
-    public static List<String> setPermutations(int n) {
-        Solver<Integer> mySolver = new Solver<>();
-
-        Integer[][] domain = new Integer[n][n];
-
-        for (int i = 0; i < n; i++) {
-            Integer[] choice = new Integer[n];
-            for (int j = 0; j<n; j++) {
-                choice[j] = j+1;
-            }
-            domain[i] = choice;
-        }
-
-        List<Lambda<Boolean, List<Integer>>> constraints = new ArrayList<>();
-
-        Lambda<Boolean, List<Integer>> lambda = x -> {
-            HashSet<Integer> in = new HashSet<>();
-            for(int i : x) {
-                if(in.contains(i)) {
-                    return true;
-                }
-                in.add(i);
-            }
-            return false;
-        };
-        constraints.add(lambda);
-
-        Set<List<Integer>> known_solutions = new HashSet<>();
-        Lambda<Boolean, List<Integer>> newConstraint = x -> {
-            if (!known_solutions.contains(x)) {
-                known_solutions.add(x);
-                return false;
-            }
-            return true;
-        };
-        constraints.add(newConstraint);
-
-        List<List<Integer>> all_solutions = mySolver.getAllSolutions(domain, constraints);
-
-        // Collect the result and convert it to the correct datastructure.
-        List<String> finalSolution = all_solutions.stream().map(characters ->
-                characters.stream().map(Object::toString).reduce((acc, e) -> acc + e).get()
-        ).sorted().collect(Collectors.toList());
-
-        System.out.println("All solutions: " + finalSolution);
-        return finalSolution;
-    }
     public static void main(String[] args) {
+        exampleProblem(3);
         //getBinaryStrings(3);
         //permutationsNoRepetitions(3, 2);
         //permutationsWithRepetitions(3, 2);
@@ -224,10 +34,11 @@ class Solution {
 }
 
 class Solver<A> {
-    public List<List<A>> getAllSolutions(A[][] domain, List<Lambda<Boolean, List<A>>> constraints) {
+    public List<List<A>> getAllSolutions(A[][] domain, List<Constraint<List<A>, Boolean>> constraints) {
         Set<List<A>> known_solutions = new HashSet<>();
         List<A> solution = getOneSolution(domain, constraints);
         while (solution != null) {
+            System.out.println("Exploring a solution: " + known_solutions.size());
             known_solutions.add(solution);
             solution = getOneSolution(domain, constraints);
         }
@@ -235,34 +46,80 @@ class Solver<A> {
         return new ArrayList<>(known_solutions);
     }
 
-    public List<A> getOneSolution(A[][] domain, List<Lambda<Boolean, List<A>>> constraints) {
-        int[] search = new int[domain.length];
+    public List<A> getOneSolution(A[][] domain, List<Constraint<List<A>, Boolean>> constraints) {
+        Integer[] search = new Integer[domain.length];
         List<A> potential_solution;
 
         do {
-            potential_solution = createPotentialSolutions(domain, search);
+            A[][] backtrackDomain = backtracking(domain, constraints, search);
+            potential_solution = createPotentialSolutions(backtrackDomain, search);
 
             if (isValid(potential_solution, constraints)) {
                 return potential_solution;
             }
-        } while (generateNext(domain, search));
+        } while (generateNext(domain, search)); // TODO: update generate next in relation to new domain setup.
 
         return null;
     }
 
-    private boolean isValid(List<A> solution, List<Lambda<Boolean, List<A>>> constraints) {
+    private boolean isValid(List<A> solution, List<Constraint<List<A>, Boolean>> constraints) {
+        System.out.println("Checking the validity of: " + solution);
         if (solution == null) {
             return false;
         }
-        for (Lambda<Boolean, List<A>> constraint : constraints) {
-            if (constraint.eval(solution)) {
+        for (Constraint<List<A>, Boolean> constraint : constraints) {
+            System.out.println("Checking constraint: " + constraint);
+            if (!constraint.eval.apply(solution)) {
+                System.out.println("Failed at: " + constraint);
                 return false;
             }
         }
+        System.out.println("Passed all checks!");
         return true;
     }
 
-    private boolean generateNext(A[][] domain, int[] previous) {
+    private A[][] backtracking(A[][] domain, List<Constraint<List<A>, Boolean>> constraints, Integer[] search) {
+        // null value in search has not been set, other values have.
+        // A: For each value that has not yet been set
+        int A = -1;
+        for (int i = 0; i < search.length; i++) {
+            if (search[i] == null) {
+                // this is A
+                A = i;
+            }
+        }
+        System.out.println("A variable that has not been set: " + A);
+        // B: If it is the only non-set value for a constraint.
+        for (var constraint : constraints) {
+            if (count_nonset_variable(constraint, search) == 1) { // find out if our variable is the only nonset variable.
+                for (int i = 0; i < domain[A].length; i++) {
+                    System.out.println("Checking: " + domain[A][i]);
+                    Integer tmp = search[A];
+                    search[A] = i;
+                    if (isValid(createPotentialSolutions(domain, search), constraints)) {
+                        System.out.println("Invalid solution, removing from domain!");
+                    }
+                    search[A] = tmp;
+                }
+            }
+        }
+        // Exhaustively search through all possible values.
+        // Remove from the domain those that do not lead to a possible solution.
+
+        return domain;
+    }
+
+    private int count_nonset_variable(Constraint<List<A>, Boolean> constraint, Integer[] search) {
+        int count = 0;
+        for (int i = 0; i < search.length; i++) {
+            if (Arrays.stream(constraint.variables).anyMatch(search[i]::equals) && search[i] == null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean generateNext(A[][] domain, Integer[] previous) {
         for (int i = domain.length-1; i >= 0; --i) {
             if (previous[i] >= domain[i].length-1) {
                 previous[i] = 0;
@@ -274,7 +131,7 @@ class Solver<A> {
         return false;
     }
 
-    private List<A> createPotentialSolutions(A[][] domain, int[] search) {
+    private List<A> createPotentialSolutions(A[][] domain, Integer[] search) {
         List<A> potential_solution = new ArrayList<>();
         for (int index = 0; index < search.length; index++) {
             potential_solution.add(domain[index][search[index]]);
@@ -286,4 +143,14 @@ class Solver<A> {
 
 interface Lambda<A, B> {
     A eval(B a);
+}
+
+class Constraint<A, B> {
+    Function<A, B> eval;
+    int[] variables;
+
+    public Constraint(Function<A, B> func, int[] variables) {
+        this.eval = func;
+        this.variables = variables;
+    }
 }
